755bb716e540ca7ea69611ab2d422c6961d0274a
amansj <apkaman9@gmail.com>
2018-03-10 16:43:13 +0530

    Update@10-03-2018

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.io.ObjectInputStream;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.commons.lang.mutable.MutableInt;
+
 public class Main {
+	private static void pageCalculations(MutableInt numOfRecord,MutableInt numPages,MutableInt remainingRecord,int numOfThread,File file) {
+		numOfRecord.setValue(countLineNumber(file));
+		numPages.setValue((numOfRecord.intValue())/numOfThread);
+		remainingRecord.setValue((numOfRecord.intValue())-numOfThread*(numPages.intValue()));	
+	}
 	private static int countLineNumber(File file)
 	{
 		int lines = 0;
@@ -23,18 +34,50 @@ public class Main {
 		return lines-1;
 	}
 	private static final int NO_OF_CORES=2;
+	@SuppressWarnings("unchecked")
+	public static void deSerialize() {
+		HashMap<Long, int[]> e = null;
+		 try
+		 {
+		 FileInputStream fileIn = new FileInputStream("ser_files/write_record.ser");
+		 ObjectInputStream in = new ObjectInputStream(fileIn);
+		 e = (HashMap<Long, int[]>) in.readObject();
+		 in.close();
+		 fileIn.close();
+		 }catch(IOException i)
+		 {
+		 i.printStackTrace();
+		 return;
+		 }catch(ClassNotFoundException c)
+		 {
+		 System.out.println("Employee class not found");
+		 c.printStackTrace();
+		 return;
+		 }
+		 for(Map.Entry<Long, int[]> entry:e.entrySet()){    
+		        Long key=entry.getKey();  
+		        int[] b=entry.getValue();  
+		        System.out.println(key+" Details:");  
+		        System.out.println(b[0]+" "+b[1]+" "+b[2]);   
+		    }
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		deSerialize();
 		System.out.print("Enter Number of Thread:");
+		HashMap<Long,int[]> threadStatus=new HashMap<Long,int[]>();
 		Scanner sc=new Scanner(System.in);
 		int numOfThread=sc.nextInt();
 		Instant starttime = Instant.now();
-		
 		String fileName="doc/pg5.csv";
 		File file=new File(fileName);
-		int numOfRecord=countLineNumber(file);
-		int numPages=numOfRecord/numOfThread;
-		int remainingRecord=numOfRecord-numOfThread*numPages;
+		MutableInt numRecords = new MutableInt();
+		MutableInt numPage=new MutableInt();
+		MutableInt remRecords=new MutableInt();
+		pageCalculations(numRecords,numPage,remRecords,numOfThread,file);
+		int numOfRecord=numRecords.intValue();
+		int numPages=numPage.intValue();
+		int remainingRecord=remRecords.intValue();
 		ExecutorService execService=Executors.newFixedThreadPool(NO_OF_CORES);
 		int start,end;
 		int i;
@@ -43,7 +86,8 @@ public class Main {
 			start=i*numPages+1;
 			end=start+numPages-1;
 			WriterThread thread=new WriterThread(fileName);
-			thread.setIndex(start, end,i);
+			threadStatus.put((long) thread.hashCode(),new int[] {start,end,0});
+			thread.setIndex(start, end,threadStatus);
 			execService.execute(thread);
 		}
 		while(remainingRecord!=0)
@@ -51,7 +95,8 @@ public class Main {
 			start=numOfRecord-remainingRecord+1;
 			end=numOfRecord-remainingRecord+1;
 			WriterThread thread=new WriterThread(fileName);
-			thread.setIndex(start, end,++i);
+			threadStatus.put((long) thread.hashCode(),new int[] {start,end,0});
+			thread.setIndex(start, end,threadStatus);
 			execService.execute(thread);
 			remainingRecord--;
 		}
@@ -61,8 +106,5 @@ public class Main {
 		sc.close();
 		Instant endtime = Instant.now();
 		System.out.println(Duration.between(starttime, endtime));
-		
-			
-	
 	}
 }
--- src/WriterThread.java
+++ src/WriterThread.java
@@ -1,83 +1,108 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 public class WriterThread implements Runnable {
 	String fileName;
+	HashMap<Long, int[]> threadStatus;
 	int start,end;
-	public void run(){  
+	private void serialize()
+	{
+		FileOutputStream fileOut;
+		try {
+		
+			fileOut = new FileOutputStream("ser_files/write_record.ser");
+			 ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			 out.writeObject(threadStatus);
+			 out.close();
+			 fileOut.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); 	
+		}
+	}
+	public void run()
+	{  
 		Connection con=null;
 		CSVReader csvReader=null;
-		 		try {
-		 		     Reader reader = Files.newBufferedReader(Paths.get(fileName));		 		    
-		 		     csvReader =new CSVReaderBuilder(reader).withSkipLines(start).build();  		 		     
-		 		     con=C3P0DataSource.getInstance().getConnection();
-		 		     PreparedStatement ps=null;
-		 		     for(long i=start;i<=end;i++)
-		 		     {  
-					    String[] data=csvReader.readNext();
-					    try {
-					    ps=con.prepareStatement("insert into pg values(?,?,?,?,?)");
-					    ps.setInt(1,Integer.parseInt(data[0]));
-					    ps.setString(2,(data[1]));
-					    ps.setString(3,(data[2]));
-					    ps.setString(4,(data[3]));
-					    ps.setInt(5,Integer.parseInt(data[4]));
-					    int r=ps.executeUpdate();
-					    if(r==1) {
-					    	System.out.println("/****************************************Processed  " +i+ "th Record********************************************************************************/");
-					    	}
-					    }
-					    catch (SQLException e) {
-							// TODO Auto-generated catch block
-					    	e.printStackTrace();
-						}
-					    finally {
-						   try {
-							   if(ps!=null)
-							   {
-								   ps.close();
-							   }
-						   } catch (SQLException e) {
-							// TODO Auto-generated catch block
-							   e.printStackTrace();
-						   }
-					   	} 
-					}
-			   }
-		 		catch (Exception e) {
+ 		try {
+ 			Reader reader = Files.newBufferedReader(Paths.get(fileName));		 		    
+ 			csvReader =new CSVReaderBuilder(reader).withSkipLines(start).build();  		 		     
+ 			con=C3P0DataSource.getInstance().getConnection();
+ 			PreparedStatement ps=null;
+ 			for(long i=start;i<=end;i++)
+ 			{  
+ 				String[] data=csvReader.readNext();
+			    try 
+			    {
+			    	ps=con.prepareStatement("insert into pg values(?,?,?,?,?)");
+				    ps.setInt(1,Integer.parseInt(data[0]));
+				    ps.setString(2,(data[1]));
+				    ps.setString(3,(data[2]));
+				    ps.setString(4,(data[3]));
+				    ps.setInt(5,Integer.parseInt(data[4]));
+				    int r=ps.executeUpdate();
+				    if(r==1) {
+				    	int[] recordStatus=threadStatus.get((long)this.hashCode());
+						recordStatus[2]+=1;
+				    	System.out.println("/****************************************Processed  " +i+ "th Record********************************************************************************/");
+			    	}
+			    }
+			    catch (SQLException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+			    	e.printStackTrace();
 				}
-			   finally {
-				try {
-					if(con!=null)
-					{
-						con.close();
+			    finally {
+			    	try {
+					   if(ps!=null)
+					   {
+						   ps.close();
+						   serialize();
+					   }
+					} 
+			    	catch (SQLException e) {
+						// TODO Auto-generated catch block
+				   		e.printStackTrace();
 					}
-					if(csvReader!=null)
-					{
-						csvReader.close();
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			   }
-			
-		  }  
-	public void setIndex(int s,int e,int id)
+			    	
+   				} 
+			}
+	   	}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+   		}
+ 		finally 
+ 		{
+ 			try {
+ 				if(con!=null)
+ 				{
+ 					con.close();
+ 				}
+ 				if(csvReader!=null)
+ 				{
+ 					csvReader.close();
+ 				}
+ 			} catch (SQLException e) {
+ 				// TODO Auto-generated catch block
+ 				e.printStackTrace();
+ 			} catch (IOException e) {
+ 				// TODO Auto-generated catch block
+ 				e.printStackTrace();
+ 			}
+	   }			
+	}  
+	public void setIndex(int s,int e,HashMap<Long, int[]> threadStatus)
 	{
-		
+		this.threadStatus=threadStatus;
 		start=s;
 		end=e;
 	}
